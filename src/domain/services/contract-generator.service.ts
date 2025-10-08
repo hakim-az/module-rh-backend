66aa9263bfb3890ce8ca0236c60e07d904ba6d58
hakim-az <hakimazzaz93@gmail.com>
2025-10-08 15:57:36 +0200

    feat: updating AP contract template

--- src/domain/services/contract-generator.service.ts
+++ src/domain/services/contract-generator.service.ts
@@ -9,8 +9,6 @@ import { nationalitiesData } from "@/application/__mock__/nationalities";
 export class ContractGeneratorService {
   private readonly templateFR = "templates/template-contart-FR.pdf";
   private readonly templateAP = "templates/template-contart-AP.pdf";
-  // private readonly templateCdiFT = "templates/template-contrat-cdi-FT.pdf";
-  private readonly templateCdiAP = "templates/template-contrat-cdi-AP.pdf";
   private readonly templateCdiMT = "templates/template-contrat-cdi-MT.pdf";
   private readonly templateCdiTéléconseillerFR =
     "templates/template-contrat-téléconseiller-FR.pdf";
@@ -18,6 +16,7 @@ export class ContractGeneratorService {
   // ---------------- v2 templates -------------------------
   private readonly templateCdiWC = "templates-v2/template-cdi-wc-v2.pdf";
   private readonly templateCdiFT = "templates-v2/template-cdi-ft-v2.pdf";
+  private readonly templateCdiAP = "templates-v2/template-cdi-ap-v2.pdf";
 
   constructor(
     private readonly s3Service: S3Service,
@@ -279,203 +278,6 @@ export class ContractGeneratorService {
     page5.drawText(`${formatDateFr(new Date())}`, {
       x: 185,
       y: 115,
-      size: 11,
-      font: fontBold,
-    });
-
-    const pdfBytes = await pdfDoc.save();
-    return Buffer.from(pdfBytes);
-  }
-
-  async generateActionPrevoyancCDIContract(contart: any): Promise<Buffer> {
-    let templateBytes: Buffer;
-
-    // Get user who owns the contract
-    const user = await this.getUserUseCase.execute(contart.idUser);
-
-    try {
-      // ✅ Fetch the template from AWS S3 instead of local file
-      templateBytes = await this.s3Service.downloadFile(this.templateCdiAP);
-    } catch {
-      throw new NotFoundException(
-        `Template PDF non trouvé dans S3 : ${this.templateCdiAP}`
-      );
-    }
-
-    const pdfDoc = await PDFDocument.load(templateBytes);
-    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
-    const fontBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
-
-    const dateNaissanceFormatted = user.naissance.dateDeNaissance
-      ? new Intl.DateTimeFormat("fr-FR", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-          timeZone: "Europe/Paris",
-        }).format(new Date(user.naissance.dateDeNaissance))
-      : "";
-
-    const dateDebutFormatted = contart.dateDebut
-      ? new Intl.DateTimeFormat("fr-FR", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-          timeZone: "Europe/Paris",
-        }).format(new Date(contart.dateDebut))
-      : "";
-
-    const dateFinFormatted = contart.dateFin
-      ? new Intl.DateTimeFormat("fr-FR", {
-          day: "2-digit",
-          month: "2-digit",
-          year: "numeric",
-          timeZone: "Europe/Paris",
-        }).format(new Date(contart.dateDebut))
-      : "";
-
-    // 🔎 Trouver la nationalité à partir du code (value) et la mettre en "Capitalized"
-    const nationality = nationalitiesData.find(
-      (n) => n.value === String(user.naissance.paysDeNaissance)
-    )
-      ? nationalitiesData
-          .find((n) => n.value === String(user.naissance.paysDeNaissance))!
-          .label.charAt(0)
-          .toUpperCase() +
-        nationalitiesData
-          .find((n) => n.value === String(user.naissance.paysDeNaissance))!
-          .label.slice(1)
-          .toLowerCase()
-      : "";
-
-    // all pages
-    const page1 = pdfDoc.getPage(0);
-    const page2 = pdfDoc.getPage(1);
-    const page4 = pdfDoc.getPage(3);
-    const page5 = pdfDoc.getPage(4);
-
-    // ------------------------------------------------ PAGE 01 ---------------------------------------------
-    // PAGE 1 (index 1) -- GOOD
-    const civiliteLabel = user.civilite === "m" ? "Monsieur" : "Madame";
-    const ne = user.civilite === "m" ? "né" : "née";
-    const immatricule = user.civilite === "m" ? "Immatriculé" : "Immatriculée";
-
-    page1.drawText(
-      `${civiliteLabel} ${user.nomDeNaissance} ${user.prenom}, ${ne} le ${dateNaissanceFormatted} à ${user.naissance.communeDeNaissance}, de nationalité ${nationality},\n${immatricule} à la sécurité sociale sous le numéro ${user.numeroSecuriteSociale}, \nDemeurant au ${user.adresse.adresse} - ${user.adresse.codePostal} ${user.adresse.ville}.`,
-      { x: 37, y: 490, size: 11, font, lineHeight: 14 }
-    );
-
-    // PAGE 1 (index 2) -- GOOD
-    page1.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 390,
-      y: 337,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 1 (index 3)
-    page1.drawText(`${contart.poste}`, {
-      x: 345,
-      y: 325,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 1 (index 4)
-    page1.drawText(`${dateDebutFormatted}`, {
-      x: 215,
-      y: 286,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 1 (index 5)
-    page1.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 370,
-      y: 286,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 1 (index 6)
-    page1.drawText(`${contart.poste}`, {
-      x: 360,
-      y: 260,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 1 (index 7) GOOD
-    page1.drawText(`${contart.missions}`, {
-      x: 40,
-      y: 190,
-      size: 11,
-      font,
-      lineHeight: 8,
-    });
-
-    // ------------------------------------------------PAGE 02 ---------------------------------------------
-    // PAGE 2 (index 1)
-    page2.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 90,
-      y: 723,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 2 (index 2)
-    page2.drawText(`${dateDebutFormatted}`, {
-      x: 330,
-      y: 723,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 2 (index 3)
-    page2.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 200,
-      y: 698,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 2 (index 4) -- GOOD
-    page2.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 85,
-      y: 132,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 2 (index 5) -- GOOD
-    page2.drawText(`${contart.salaire} €`, {
-      x: 510,
-      y: 132,
-      size: 11,
-      font: fontBold,
-    });
-
-    // ------------------------------------------------PAGE 04 ---------------------------------------------
-    // PAGE 4 (index 1)
-    page4.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 275,
-      y: 494,
-      size: 11,
-      font: fontBold,
-    });
-
-    // ------------------------------------------------PAGE 05 ---------------------------------------------
-    // PAGE 5 (index 1)
-    page5.drawText(`${formatDateFr(new Date())}`, {
-      x: 340,
-      y: 582,
-      size: 11,
-      font: fontBold,
-    });
-
-    // PAGE 5 (index 2)
-    page5.drawText(`${user.nomDeNaissance} ${user.prenom}`, {
-      x: 85,
-      y: 507,
       size: 11,
       font: fontBold,
     });
@@ -1029,6 +831,189 @@ export class ContractGeneratorService {
       {
         x: 37,
         y: 550,
+        size: 12,
+        font: fontBold,
+        lineHeight: 14,
+      }
+    );
+
+    const pdfBytes = await pdfDoc.save();
+    return Buffer.from(pdfBytes);
+  }
+
+  async generateActionPrevoyancCDIContract(contart: any): Promise<Buffer> {
+    let templateBytes: Buffer;
+
+    // Get user who owns the contract
+    const user = await this.getUserUseCase.execute(contart.idUser);
+
+    try {
+      // ✅ Fetch the template from AWS S3 instead of local file
+      templateBytes = await this.s3Service.downloadFile(this.templateCdiAP);
+    } catch {
+      throw new NotFoundException(
+        `Template PDF non trouvé dans S3 : ${this.templateCdiAP}`
+      );
+    }
+
+    const pdfDoc = await PDFDocument.load(templateBytes);
+    const font = await pdfDoc.embedFont(StandardFonts.Helvetica);
+    const fontBold = await pdfDoc.embedFont(StandardFonts.HelveticaBold);
+
+    const dateNaissanceFormatted = user.naissance.dateDeNaissance
+      ? new Intl.DateTimeFormat("fr-FR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          timeZone: "Europe/Paris",
+        }).format(new Date(user.naissance.dateDeNaissance))
+      : "";
+
+    const dateDebutFormatted = contart.dateDebut
+      ? new Intl.DateTimeFormat("fr-FR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          timeZone: "Europe/Paris",
+        }).format(new Date(contart.dateDebut))
+      : "";
+
+    const dateFinFormatted = contart.dateFin
+      ? new Intl.DateTimeFormat("fr-FR", {
+          day: "2-digit",
+          month: "2-digit",
+          year: "numeric",
+          timeZone: "Europe/Paris",
+        }).format(new Date(contart.dateDebut))
+      : "";
+
+    // 🔎 Trouver la nationalité à partir du code (value) et la mettre en "Capitalized"
+    const nationality = nationalitiesData.find(
+      (n) => n.value === String(user.naissance.paysDeNaissance)
+    )
+      ? nationalitiesData
+          .find((n) => n.value === String(user.naissance.paysDeNaissance))!
+          .label.charAt(0)
+          .toUpperCase() +
+        nationalitiesData
+          .find((n) => n.value === String(user.naissance.paysDeNaissance))!
+          .label.slice(1)
+          .toLowerCase()
+      : "";
+
+    // all pages
+    const page1 = pdfDoc.getPage(0);
+    const page2 = pdfDoc.getPage(1);
+    const page5 = pdfDoc.getPage(4);
+
+    // ------------------------------------------------PAGE 01 ---------------------------------------------
+    // PAGE 1 (index 1) -- good
+    const civiliteLabel = user.civilite === "m" ? "Monsieur" : "Madame";
+    const ne = user.civilite === "m" ? "né" : "née";
+    const immatricule = user.civilite === "m" ? "Immatriculé" : "Immatriculée";
+
+    const description = wrapTextByLength(
+      `${civiliteLabel} ${user.nomDeNaissance.toUpperCase()} ${user.prenom.charAt(0).toUpperCase() + user.prenom.slice(1).toLowerCase()}, ${ne} le ${dateNaissanceFormatted} à ${user.naissance.communeDeNaissance}, de nationalité ${nationality}, ${immatricule} à la sécurité sociale sous le numéro ${user.numeroSecuriteSociale}, Demeurant au ${user.adresse.adresse} - ${user.adresse.codePostal} ${user.adresse.ville}.`,
+      100
+    );
+
+    page1.drawText(description, {
+      x: 37,
+      y: 500,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    // PAGE 1 (index 2)
+    const engagamentFirst = wrapTextByLength(
+      `Sous réserve des résultats de la visite médicale d'embauche, le salarié est engagé par la société ACTION PRÉVOYANCE, en qualité de ${contart.poste} dans les conditions prévues par les articles L. 7311-1 et suivants du code du travail.`,
+      100
+    );
+
+    const engagamentSecond = wrapTextByLength(
+      `Le présent contrat prendra effet le ${dateDebutFormatted}. Le salarié déclare expressément ce jour ne pas être débiteur d'une obligation de non-concurrence incompatible avec son engagement par la société ACTION PRÉVOYANCE en qualité de ${contart.poste}.`,
+      100
+    );
+
+    page1.drawText(engagamentFirst, {
+      x: 37,
+      y: 350,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    page1.drawText(engagamentSecond, {
+      x: 37,
+      y: 300,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    // PAGE 1 (index 3)
+    page1.drawText(contart.missions, {
+      x: 37,
+      y: 190,
+      size: 11,
+      font,
+      lineHeight: 8,
+    });
+
+    // ------------------------------------------------PAGE 02 ---------------------------------------------
+    // PAGE 2 (index 1)
+    const dureeFirst = `Le salarié est engagé le ${dateDebutFormatted} à durée indéterminée.`;
+
+    const dureeSecond = wrapTextByLength(
+      `Compte tenu du fait que le salarié a précédemment effectué un stage au sein de la société sur des missions identiques à celles prévues dans le cadre du présent contrat, aucune période d'essai ne sera prévue, conformément à la jurisprudence et à l'esprit de l'article L.1221-19 du Code du travail.`,
+      100
+    );
+
+    page2.drawText(dureeFirst, {
+      x: 37,
+      y: 725,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    page2.drawText(dureeSecond, {
+      x: 37,
+      y: 705,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    // PAGE 2 (index 2)
+    const remuniration = `Le salarié percevra une rémunération mensuelle brute fixée à ${contart.salaire} €.`;
+
+    page2.drawText(remuniration, {
+      x: 37,
+      y: 140,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    // ------------------------------------------------PAGE 05 ---------------------------------------------
+    // PAGE 5 (index 1)
+    const currentDate = `Fait en deux exemplaires originaux à Fontenay-Sous-Bois, le ${formatDateFr(new Date())}`;
+
+    page5.drawText(currentDate, {
+      x: 37,
+      y: 575,
+      size: 11,
+      font,
+      lineHeight: 14,
+    });
+
+    page5.drawText(
+      `${user.nomDeNaissance.toUpperCase()} ${user.prenom.charAt(0).toUpperCase() + user.prenom.slice(1).toLowerCase()}`,
+      {
+        x: 37,
+        y: 505,
         size: 12,
         font: fontBold,
         lineHeight: 14,
